43ded2b3250066de14b09ee6cf21ed33a8648e04
Ray-Yuan Liu <louis383@gmail.com>
2014-07-12 12:45:13 +0800

    Init ProjectOil

---
+++ core/src/com/raysgame/projecoil/ProjectOil.java
@@ -0,0 +1,13 @@
+package com.raysgame.projecoil;
+
+import com.badlogic.gdx.Game;
+
+public class ProjectOil extends Game{
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ desktop/src/com/raysgame/projecoil/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.raysgame.projecoil.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.raysgame.projecoil.Projectoil;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Projectoil(), config);
+	}
+}
