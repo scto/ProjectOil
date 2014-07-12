9bb3058ac192dc8532b8142e262931806df23fa1
Ray-Yuan Liu <louis383@gmail.com>
2014-07-12 22:37:14 +0800

    AirPlane Object. And It Can Controll.

--- core/src/com/raysgame/projecoil/Assets.java
+++ core/src/com/raysgame/projecoil/Assets.java
@@ -8,10 +8,19 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class Assets {
 	public static Texture textureBackground;    //圖片檔本身
 	public static Sprite spriteBackgound;    //Game Object
+	
+	public static Texture textureAirPlane;
+	public static Sprite spriteAirPlane;
+	
     public static void load() {
-    	textureBackground = new Texture(Gdx.files.internal("menu/back1.png"));    //設定textureBackground路徑
+    	textureBackground = new Texture(Gdx.files.internal("data/sky.png"));    //設定textureBackground路徑
 		textureBackground.setFilter(TextureFilter.Linear, TextureFilter.Linear);     //我猜是拉申用的
 		spriteBackgound = new Sprite(textureBackground);
 		spriteBackgound.flip(false, true);
+		
+		//Picture for airplane
+		textureAirPlane = new Texture(Gdx.files.internal("data/airplane_template.png"));
+		spriteAirPlane = new Sprite(textureAirPlane);
+		spriteAirPlane.flip(false, true);
     }
 }
--- core/src/com/raysgame/projecoil/GameScreen.java
+++ core/src/com/raysgame/projecoil/GameScreen.java
@@ -2,6 +2,7 @@ package com.raysgame.projecoil;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,7 +11,7 @@ public class GameScreen implements Screen{
 	ProjectOil games;
 	OrthographicCamera camera;
 	SpriteBatch batch;
-
+    int characterX, characterY;
 	
 	public GameScreen(ProjectOil _games) {
 		this.games = _games;
@@ -21,6 +22,9 @@ public class GameScreen implements Screen{
 		//true 是為了翻轉座標系統 libGDX 用的跟一般數學是一樣的，所以要翻轉
 		
 		batch = new SpriteBatch();    //For Image Rendering
+		characterX = 960 - 64;    //一開始的X軸位置
+		characterY = 540;
+		
 	}
 
 	@Override
@@ -30,13 +34,29 @@ public class GameScreen implements Screen{
 		
 		camera.update();    //60fps 速率更新鏡頭
 		batch.setProjectionMatrix(camera.combined);    //遊戲視窗內圖片大小縮放
-		
+		generalUpdate();    //控制移動
 		batch.begin();
 		//Rendering Code Here
 		batch.draw(Assets.spriteBackgound, 0, 0);
+		batch.draw(Assets.spriteAirPlane, characterX, characterY);
 		batch.end();
 	}
 
+	public void generalUpdate() {
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+		    characterX -= 5;	
+		} 
+		else if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			characterX += 5;
+		}
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			characterY -= 5;
+		}
+		else if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+			characterY += 5;
+		}
+	}
+	
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
